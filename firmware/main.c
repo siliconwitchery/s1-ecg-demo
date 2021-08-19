dbb22a1019fd389afae9f6b211a12cbe9f285947
Rohit Rathnam N <rohitrathnam.n@gmail.com>
2021-08-19 18:17:50 +0200

    Simple FIR filter works on ADC data

--- firmware/main.c
+++ firmware/main.c
@@ -37,17 +37,25 @@
 #include "nrfx_spim.h"
 #include "nrfx_twim.h"
 #include "s1.h"
+#include "fir_filter.h"
 
 #define SPI_SI_PIN NRF_GPIO_PIN_MAP(0, 8)
 #define SPI_SO_PIN NRF_GPIO_PIN_MAP(0, 11)
 #define SPI_CS_PIN NRF_GPIO_PIN_MAP(0, 12)
 #define SPI_CLK_PIN NRF_GPIO_PIN_MAP(0, 15)
 
-static const nrfx_spim_t spi = NRFX_SPIM_INSTANCE(0);
 static nrf_saadc_value_t m_buffer;
+static uint32_t SAMPLES_IN_BUFFER = 1;
+static uint32_t m_adc_evt_counter;
+static int32_t adc_val;
+
+static float buffer[65];
+static int32_t buff_idx = 0;
+static float filtered_val;
 
 APP_TIMER_DEF(fpga_boot_task_id);
 APP_TIMER_DEF(adc_task_id);
+APP_TIMER_DEF(filter_task_id);
 
 typedef enum
 {
@@ -97,7 +105,7 @@ static void fpga_boot_task(void *p_context)
         {
             pages_remaining = (uint32_t)ceil((float)fpga_binfile_bin_len / 256.0);
             fpga_boot_state = FLASHING;
-            LOG("Flashing %d pages.", pages_remaining);
+            // LOG("Flashing %d pages.", pages_remaining);
         }
         break;
 
@@ -114,7 +122,7 @@ static void fpga_boot_task(void *p_context)
         {
             fpga_boot_state = BOOTING;
             s1_fpga_boot();
-            LOG("Flashing done.");
+            // LOG("Flashing done.");
             break;
         }
         break;
@@ -125,40 +133,117 @@ static void fpga_boot_task(void *p_context)
         {
             app_timer_stop(fpga_boot_task_id);
             fpga_boot_state = DONE;
-            LOG("FPGA started.");
+            // LOG("FPGA started.");
         }
         break;
     }
 }
 
-/**
- * @brief Timer based function for ADC read
- */
-static void adc_task(void *p_context)
+void graph_to_log(uint32_t value)
 {
-    UNUSED_PARAMETER(p_context);
+    const uint32_t steps = 30;
+    uint32_t idx = value / floor(4095.0 / steps);
+    // LOG_RAW("%d__|", idx);
+    LOG_RAW(" |");
+    if (idx > 0)
+    {
+        for (int i = 0; i < idx; i++)
+        {
+            LOG_RAW(" ");
+        }
+    }
+    LOG_RAW("*");
+    if (idx < steps)
+    {
+        for (int i = idx + 1; i < steps + 1; i++)
+        {
+            LOG_RAW(" ");
+        }
+    }
+    float scaled_value = value / 4095.0 * 1.8;
+    LOG_RAW("| %.2f \r\n", scaled_value);
 }
 
-static void fpga_tx_rx(uint8_t *tx_buffer, size_t tx_len,
-                       uint8_t *rx_buffer, size_t rx_len)
+void saadc_callback(nrfx_saadc_evt_t const *p_event)
 {
-    // SPI hardware configuration
-    nrfx_spim_config_t spi_config = NRFX_SPIM_DEFAULT_CONFIG;
+    if (p_event->type == NRFX_SAADC_EVT_DONE)
+    {
+        ret_code_t err_code;
 
-    // Use active high logic on CS to prevent flash from responding
-    spi_config.ss_active_high = 1;
-    spi_config.mosi_pin = SPI_SO_PIN;
-    spi_config.miso_pin = SPI_SI_PIN;
-    spi_config.sck_pin = SPI_CLK_PIN;
-    spi_config.ss_pin = SPI_CS_PIN;
+        // Get SAMPLES_IN_BUFFER x samples when its time
+        err_code = nrfx_saadc_buffer_convert(p_event->data.done.p_buffer, SAMPLES_IN_BUFFER);
+        APP_ERROR_CHECK(err_code);
 
-    // Initialise the SPI if it was not already
-    nrfx_spim_init(&spi, &spi_config, NULL, NULL);
+        // Average the samples
+        adc_val = 0;
+        for (int i = 0; i < SAMPLES_IN_BUFFER; i++)
+        {
+            adc_val = adc_val + p_event->data.done.p_buffer[i];
+        }
+        adc_val = adc_val / SAMPLES_IN_BUFFER; // note adc_val is int
 
-    nrfx_spim_xfer_desc_t spi_xfer = NRFX_SPIM_XFER_TRX(tx_buffer, tx_len,
-                                                        rx_buffer, rx_len);
+        // TODO: Add ADC calibration here, clipping negatives to 0
+        if (adc_val < 0)
+            adc_val = 0;
+        m_adc_evt_counter++;
+    }
+}
 
-    APP_ERROR_CHECK(nrfx_spim_xfer(&spi, &spi_xfer, 0));
+void saadc_init(void)
+{
+    ret_code_t err_code;
+
+    nrfx_saadc_config_t adc_config = {
+        .resolution = NRF_SAADC_RESOLUTION_12BIT,
+        .oversample = NRF_SAADC_OVERSAMPLE_DISABLED};
+
+    nrf_saadc_channel_config_t channel_config = {
+        .resistor_p = NRF_SAADC_RESISTOR_DISABLED,
+        .resistor_n = NRF_SAADC_RESISTOR_DISABLED,
+        .gain = NRF_SAADC_GAIN1_3,                 // so that Vmax/gain = 0.6
+        .reference = NRF_SAADC_REFERENCE_INTERNAL, // internal ref 0.6V
+        .acq_time = NRF_SAADC_ACQTIME_3US,
+        .mode = NRF_SAADC_MODE_SINGLE_ENDED,
+        .burst = NRF_SAADC_BURST_DISABLED,
+        .pin_p = NRF_SAADC_INPUT_AIN2,
+        .pin_n = NRF_SAADC_INPUT_DISABLED};
+
+    err_code = nrfx_saadc_init(&adc_config, saadc_callback);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrfx_saadc_channel_init(0, &channel_config);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrfx_saadc_buffer_convert(&m_buffer, SAMPLES_IN_BUFFER);
+    APP_ERROR_CHECK(err_code);
+}
+
+void saadc_task(void *p_context)
+{
+    // LOG("# %u %d", m_adc_evt_counter, adc_val);
+    // graph_to_log((uint32_t)adc_val);
+
+    ret_code_t err_code;
+    err_code = nrfx_saadc_sample();
+    APP_ERROR_CHECK(err_code);
+}
+
+void filter_task(void *p_context)
+{
+    buffer[buff_idx] = adc_val;
+    if (buff_idx == 64)
+        buff_idx = 0;
+    else
+        buff_idx = buff_idx + 1;
+    filtered_val = 0;
+    for (int i = 0; i < 65; i++)
+    {
+        if (i + buff_idx < 65)
+            filtered_val += buffer[i + buff_idx] * fir_coefs[i];
+        else
+            filtered_val += buffer[i + buff_idx - 65] * fir_coefs[i];
+    }
+    graph_to_log((uint32_t)filtered_val);
 }
 
 /**
@@ -184,6 +269,9 @@ int main(void)
     APP_ERROR_CHECK(app_timer_init());
     APP_SCHED_INIT(sizeof(uint32_t), 5);
 
+    // Initialise ADC
+    saadc_init();
+
     // Create and start a timer for the FPGA flash/boot task
     APP_ERROR_CHECK(app_timer_create(&fpga_boot_task_id,
                                      APP_TIMER_MODE_REPEATED,
@@ -193,12 +281,22 @@ int main(void)
                                     APP_TIMER_TICKS(5),
                                     NULL));
 
+    // Create timer for ADC
     APP_ERROR_CHECK(app_timer_create(&adc_task_id,
                                      APP_TIMER_MODE_REPEATED,
-                                     adc_task));
-
+                                     saadc_task));
+    // Run ADC at 125Hz -> 1000 / 125 = 8ms
     APP_ERROR_CHECK(app_timer_start(adc_task_id,
-                                    APP_TIMER_TICKS(100),
+                                    APP_TIMER_TICKS(8),
+                                    NULL));
+
+    // Create timer for filter
+    APP_ERROR_CHECK(app_timer_create(&filter_task_id,
+                                     APP_TIMER_MODE_REPEATED,
+                                     filter_task));
+
+    APP_ERROR_CHECK(app_timer_start(filter_task_id,
+                                    APP_TIMER_TICKS(8),
                                     NULL));
 
     // The CPU is free to do nothing in the meanwhile
